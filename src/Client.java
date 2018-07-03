2fc8e52efff20bd815d4c84c1d2bba701d8cb429
Seth Reimer <seth.reimer@appian.com>
2018-07-03 14:09:28 -0400

    Working session 3. Broadcast message to client, only 1 client works

--- src/Client.java
+++ src/Client.java
@@ -1,18 +1,48 @@
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
 	private static final int SERVER_PORT_NUMBER = 4000;
+	private static Socket serverSocket;
+
+	private static class Listener implements Runnable {
+		Socket socket;
+
+		private Listener(Socket socket) {this.socket = socket;}
+
+		@Override
+		public void run() {
+			while(true) {
+				try {
+					DataInputStream inputSteam = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+					System.out.println("Received from server: " + inputSteam.readUTF());
+				} catch (IOException e) {
+				  break;
+				}
+			}
+      try {
+        serverSocket.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+	}
 
 	public static void main(String[] args) throws Exception {
 		final InetAddress SERVER_ADDRESS = InetAddress.getByName("localhost");
-		Socket serverSocket = new Socket(SERVER_ADDRESS, SERVER_PORT_NUMBER);
+		serverSocket = new Socket(SERVER_ADDRESS, SERVER_PORT_NUMBER);
+		Listener listener = new Listener(serverSocket);
+		Thread connectionThread = new Thread(listener);
+		connectionThread.start();
 		DataOutputStream oStream = new DataOutputStream(serverSocket.getOutputStream());
 		Scanner input = new Scanner(System.in);
 		while (!serverSocket.isClosed()) {
-			oStream.writeUTF(input.nextLine());
+			oStream.writeUTF(input.nextLine()); //TODO this is a blocking operation so it won't detect that the socket is closed
 		}
 	}
 }
--- src/Server.java
+++ src/Server.java
@@ -1,57 +1,77 @@
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.Socket;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Server {
 	private static final int PORT_NUMBER = 4000; //TODO do this a better way
+  private static List<ClientConnection> activeConnections;
 
   private final static class ClientConnection implements Runnable {
-    private final Socket socket;
+    private final Socket incomingSocket;
+    private DataOutputStream outputStream;
 
-    private ClientConnection(Socket connection) {
-      this.socket = connection;
+    private ClientConnection(Socket incomingSocket) {
+      this.incomingSocket = incomingSocket;
+      try {
+        this.outputStream = new DataOutputStream(incomingSocket.getOutputStream());
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
 
     @Override
     public void run() {
       try {
-        DataInputStream inputSteam = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+        DataInputStream inputSteam = new DataInputStream(new BufferedInputStream(incomingSocket.getInputStream()));
         while (true) {
           String data;
           data = inputSteam.readUTF();
-          if (data.equals("exit")) {
-            socket.close();
+          if (data.equals("exit")) { //TODO exit no longer works
+            incomingSocket.close();
             inputSteam.close();
+            outputStream.close();
+            activeConnections.remove(this);
             break;
           }
-
           System.out.println(data);
+          broadCastMessage(data);
         }
       } catch (Exception e) {
         e.printStackTrace();
       }
     }
+
+    public void sendMessage(String message) throws IOException {
+      outputStream.writeUTF(message);
+    }
   }
 
 	public static void main(String[] args) {
-    List<ClientConnection> activeConnections = new ArrayList<>();
+    activeConnections = new ArrayList<>();
     try {
       ServerSocket listeningServer = new ServerSocket(PORT_NUMBER); //We need the ServerSocket to detect the incoming connections
       while (true) {
-        Socket socket = listeningServer.accept(); //We need this socket to facilitate communication
-        System.out.println(socket);
+        Socket incomingSocket = listeningServer.accept();//We need this socket to facilitate communication
+        System.out.println(incomingSocket);
 
-        ClientConnection connection = new ClientConnection(socket);
+        ClientConnection connection = new ClientConnection(incomingSocket);
         Thread connectionThread = new Thread(connection);
         connectionThread.start();
         activeConnections.add(connection);
       }
     } catch (IOException e) {
       System.out.println(e);
+    }
+  }
+
+  public static void broadCastMessage(String message) throws IOException {
+    for (ClientConnection connection : activeConnections) {
+      connection.sendMessage(message);
     }
   }
 }
